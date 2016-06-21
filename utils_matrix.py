207c095196557bf1ebbf0c99b4aad79fc6c376ac
Ngoc Tran <ngoc@underlandian.com>
2016-06-21 15:28:00 -0500

    copied janko stuff, unfixed

--- utils_matrix.py
+++ utils_matrix.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python2
+from bs4 import BeautifulSoup
+import urllib2
+
 ''' A matrix representation of ABC Endview puzzle.
 	Ngoc Tran - 2016 || underlandian.com
 
@@ -19,22 +22,21 @@ primes = [1,2,3,5,7,11,13,17,19,23,29]
 # INITIALIZATION SECTION
 # __________
 
-''' Generate empty equation.
-	Equation has the form [[list_of_members],[list_of_primes]]
-	'''
 def generate_empty_equation():
-	# initialize new equation, [1] is the content, [2] is the result
+	''' Generate empty equation.
+		Equation has the form [[list_of_members],[list_of_primes]]
+		'''
 	new_equation = []
 	# prevent internal caching
 	for i in range(2):
 		new_equation.append([])
 	return new_equation
 
-''' Generate empty board.
-	board is a 2-dimensional nested list, with 0 denotes empty,
-		1 denotes X, and so on, as noted in primes.
-	'''
 def generate_empty_board(dim):
+	''' Generate empty board.
+		board is a 2-dimensional nested list, with 0 denotes empty,
+		1 denotes X, and so on, as noted in primes.
+		'''
 	# initialize new board
 	new_board = []
 	# prevent internal caching
@@ -46,17 +48,33 @@ def generate_empty_board(dim):
 		new_board.append(new_row)
 	return new_board
 
-''' Initialize the requirement equations with given constraint.
-	Note: skip ones with no constraints.
+def generate_empty_constraint(dim):
+	''' Generate empty constraint.
+		also a 2-dimensional nested list, same denotation with board.
+		0, 1, 2, 3 are top, bottom, left, right, respectively.
+		'''
+	# initialize new board
+	new_constraint = []
+	# prevent internal caching
+	for i in range(4):
+		new_row = []
+		for j in range(dim):
+			# append a new column
+			new_row.append(0)
+		new_constraint.append(new_row)
+	return new_constraint
 
-	board and constraint are nested list of integers.
-	choices denote the length of possible character.
-		e.g.: 3 means A, B, and C; or equivalently, 2, 3, and 5.
-	diag denotes if the board has to have diagonals.
-	'''
 # implementation note: something better than a nested list is good,
 # 	but unfortunately lists aren't hashable, and editability is required.
 def generate_equation_list(board, constraint, choices, diag):
+	''' Initialize the requirement equations with given constraint.
+		Note: skip ones with no constraints.
+
+		board and constraint are nested list of integers.
+		choices denote the length of possible character.
+			e.g.: 3 means A, B, and C; or equivalently, 2, 3, and 5.
+		diag denotes if the board has to have diagonals.
+		'''
 	equation_list = []
 	# dimension of the board
 	dim = len(board[0])
@@ -156,19 +174,18 @@ def generate_equation_list(board, constraint, choices, diag):
 # CONSTRAINT REDUCTION SECTION
 # __________
 
-''' New mulitiplication algorithm.
-	TODO: prove ring-ness or whatever. Probably will never get used.
-	'''
+#	TODO: prove ring-ness or whatever. Probably will never get used.
 def multiply(i, j):
+	''' New mulitiplication algorithm. '''
 	if i == j:
 		if i in primes and i != 1:
 			return 0
 	return i * j
 
-''' Check for obvious fills or unsolvable equations.
-	Returns whether anything is changed.
-	'''
 def equation_list_check(board, equation_list):
+	''' Check for obvious fills or unsolvable equations.
+		Returns whether anything is changed.
+		'''
 	changed = False
 	for constraint_equation in equation_list:
 		# Border constraint
@@ -194,18 +211,18 @@ def equation_list_check(board, equation_list):
 				changed = True
 	return changed
 
-''' Run constraint reduction on all equations which is in the list.
-	Empty list means board is all filled.
-
-	Status ints:
-		+1 = satisfied
-		+0 = unsatisfied
-		-1 = invalid
-
-	If constraint_list[0] is [],[-1],
-		then the board is impossible. This is to help trial and error.
-	'''
 def equation_list_reduction(board, equation_list):
+	''' Run constraint reduction on all equations which is in the list.
+		Empty list means board is all filled.
+
+		Status ints:
+			+1 = satisfied
+			+0 = unsatisfied
+			-1 = invalid
+
+		If constraint_list[0] is [],[-1],
+			then the board is impossible. This is to help trial and error.
+		'''
 	if logging:
 		print 'change detected, running equation_list_reduction'
 	for constraint_equation in equation_list:
@@ -217,15 +234,15 @@ def equation_list_reduction(board, equation_list):
 			equation_list[:] = [[[],[-1]]]
 			break
 
-''' Evaluate constraint reduction on each constraint equation.
-	If both sides are equal, return True.
-	Else, divides into border constraints and Latin constraints:
-	- If border constraint (list of primes):
-		Anything after a label different from constraint is negligible.
-	- If Latin constraint (0 in result):
-		Order does not matter.
-	'''
 def equation_reduction(board, constraint_equation):
+	''' Evaluate constraint reduction on each constraint equation.
+		If both sides are equal, return True.
+		Else, divides into border constraints and Latin constraints:
+		- If border constraint (list of primes):
+			Anything after a label different from constraint is negligible.
+		- If Latin constraint (0 in result):
+			Order does not matter.
+		'''
 	# Border constraint
 	if constraint_equation[1] == [0]:
 		for coord in constraint_equation[0][1:]:
@@ -303,10 +320,10 @@ def equation_reduction(board, constraint_equation):
 # PRINTING SECTION
 # __________
 
-''' Return one horizontal border.
-	Used in printOut.
-	'''
 def generate_horizontal_border(dim):
+	''' Return one horizontal border.
+		Used in printOut.
+		'''
     hBorder = '  '
     for i in range(dim):
         hBorder += '+-'
@@ -315,10 +332,10 @@ def generate_horizontal_border(dim):
     hBorder += '+'
     return hBorder
 
-''' Return int to char.
-	e.g., -1 is blank, 0 is - (X), 1 is A, and so on.
-	'''
 def int_to_char(i):
+	''' Return int to char.
+		e.g., -1 is blank, 0 is - (X), 1 is A, and so on.
+		'''
 	if i > 1:
 		return chr(ord('A') + primes.index(i) - 1)
 	elif i == 1:
@@ -328,10 +345,10 @@ def int_to_char(i):
 	else:
 		raise ValueError(str(i) + ' is not a valid int for a label')
 
-''' Return a pretty printing the board.
-	Reliant on '\n', be careful.
-	'''
 def stringify(board, constraint = None):
+	''' Return a pretty printing the board.
+		Reliant on '\n', be careful.
+		'''
     dim = len(board)
     ret = ''
     horizontal_border = generate_horizontal_border(dim)
@@ -374,6 +391,150 @@ def stringify(board, constraint = None):
     	ret += int_to_char(constraint[1][i])
         ret += '   '
     return ret
+
+# JANKO PARSER SECTION
+# __________
+
+def janko_get_text(no = 0):
+	''' Get the problem from janko site.
+		Will cache into a folder.
+		Note: commenting out if beautifulsoup4 is not present.
+		'''
+	if no == 0:
+        print 'Janko.at problem solver'
+        not_entered = True
+        while not_entered:
+            text = raw_input("Problem number? ")
+            try:
+                no = int(text)
+                if no > 530 or no < 1:
+                    print 'Not a valid question number!'
+                else:
+                    not_entered = False
+            except:
+                print 'Not a valid number!'
+
+    url = 'http://www.janko.at/Raetsel/Abc-End-View/'+str(no).zfill(3)+'.a.htm'
+    file = urllib2.urlopen(url)
+    html_doc = file.read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    data = soup.data.get_text().split('\n')
+
+def janko_parser(data):
+	''' Janko parser.
+		Return the board and its constraints - not equation lists.
+		'''
+    choices = ''
+    diag = False
+    partial = False
+    del data[0]
+    del data[-1]
+
+    attributes = ['size','depth','options','clabels','rlabels','problem']
+    while True:
+        while len(data) > 0:
+            if len(data[0]) == 0 or data[0].split()[0] not in attributes:
+                del data[0]
+            else:
+                break
+        if len(data) == 0:
+            break
+        if data[0].split()[0] == 'size':
+            dim = int(data[0].split()[1])
+            constraint = generate_empty_constraint(dim)
+            attributes.remove('size')
+        if data[0].split()[0] == 'depth':
+            depth = int(data[0].split()[1])
+            attributes.remove('depth')
+            for i in range(depth):
+                choices += chr(ord('A')+i)
+            if dim > len(choices):
+                choices += 'X'
+        if data[0].split()[0] == 'options':
+            attributes.remove('options')
+            diag = True
+        # top/bottom
+        if data[0].split()[0] == 'clabels':
+            attributes.remove('clabels')
+            del data[0]
+            topConstraint = data[0].upper().encode('ascii','ignore').split()
+            for i in range(dim):
+                if topConstraint[i] != '-':
+                    try:
+                        constraint[1][i][0] = chr(int(topConstraint[i])+ord('A')-1)
+                    except:
+                        constraint[1][i][0] = topConstraint[i]
+            del data[0]
+            del topConstraint
+            #__________
+            bottomConstraint = data[0].upper().encode('ascii','ignore').split()
+            for i in range(dim):
+                if bottomConstraint[i] != '-':
+                    try:
+                        constraint[1][i][1] = chr(int(bottomConstraint[i])+ord('A')-1)
+                    except:
+                        constraint[1][i][1] = bottomConstraint[i]
+            del data[0]
+            del bottomConstraint
+            #__________
+        # left/right
+        if data[0].split()[0] == 'rlabels':
+            attributes.remove('rlabels')
+            del data[0]
+            leftConstraint = data[0].upper().encode('ascii','ignore').split()
+            for i in range(dim):
+                if leftConstraint[i] != '-':
+                    try:
+                        constraint[0][i][0] = chr(int(leftConstraint[i])+ord('A')-1)
+                    except:
+                        constraint[0][i][0] = leftConstraint[i]
+            del data[0]
+            del leftConstraint
+            #__________
+            rightConstraint = data[0].upper().encode('ascii','ignore').split()
+            for i in range(dim):
+                if rightConstraint[i] != '-':
+                    try:
+                        constraint[0][i][1] = chr(int(rightConstraint[i])+ord('A')-1)
+                    except:
+                        constraint[0][i][1] = rightConstraint[i]
+            del data[0]
+            del rightConstraint
+        # problem lmao
+        if data[0].split()[0] == 'problem':
+            attributes.remove('problem')
+            del data[0]
+            partial = True
+            board = init_board(constraint, choices, diag)
+            for i in range(dim):
+                clues = data[0].upper().encode('ascii','ignore').split()
+                del data[0]
+                for j in range(dim):
+                    if clues[j] != '-':
+                        board[i][j] = clues[j]
+            cancel_all(board, constraint, choices, diag)
+            mass_optimize(board, constraint, choices, diag)
+
+    add_stat(constraint, choices, diag, partial)
+    
+    # print constraint, partial, choices
+    # printOut(init_board(constraint, choices, diag))
+    if not partial:
+        result = solve(constraint, choices, diag)
+    else:
+        result = solve_from_partial(board, constraint, choices, diag)
+    if pass_on:
+        return result
+    else:
+        solutions_list = result[0]
+        counts = result[1]
+        for i in range(len(solutions_list)):
+            print 'Solution #'+str(i+1)+"\n"
+            printOut(solutions_list[i], constraint)
+            print
+        print "total number of solutions:", len(solutions_list)
+        print "total number of trials:", counts[0]
+        print "total number of expansions:", counts[1]
 
 # WORK IN PROGRESS // TESTING SECTION
 def shabby_printing(board, constraint, equation_list):
